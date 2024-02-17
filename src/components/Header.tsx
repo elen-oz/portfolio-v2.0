a2c84442683bf5a25c99995345b83d210fa5f9f7
elen-oz <ozerova.lenka@gmail.com>
2024-02-17 23:42:59 +0100

    feat: change copyright

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,24 @@
-import { PiCopyrightLight } from 'react-icons/pi';
-
 const Header = () => {
   return (
     <header className='z-10 md:py-[30px]'>
-      <div className='mx-auto px-4 flex place-content-between'>
-        <a className='flex gap-2 bg-white'>
+      <div className='mx-auto p-2 md:px-4 flex place-content-between'>
+        <a className='flex gap-2 bg-white h-[49px]'>
           <div className=''>
-            <div>elena</div>
+            <div>elena.</div>
             <div>2024</div>
           </div>
 
-          <div>
+          <div className='relative'>
             <span>portfolio</span>
-            <PiCopyrightLight />
+
+            <div className='absolute bottom-0 right-0 self-end px-[9px] pb-[3px] border-solid border-black border-2 rounded-full'>
+              c
+            </div>
           </div>
         </a>
 
-        <nav className='bg-white'>
-          <ul className='flex flex-col text-right md:flex-row md:gap-2'>
+        <nav className='bg-white flex md:items-center'>
+          <ul className='flex flex-col  text-right md:flex-row md:gap-2 '>
             <li>Home</li>
             <li>Skills</li>
             <li>Work</li>
