9a793d5aae389fb3e7ad975c26cda2ad62ce7a98
elen-oz <ozerova.lenka@gmail.com>
2024-02-18 01:03:40 +0100

    feat: add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Header from './components/Header';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <h1 className='text-4xl'>ELENA OZEROVA</h1>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['DM Sans', 'Manrope', 'sans-serif'],
+        accent: ['Rock Salt', 'Reenie Beanie', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
