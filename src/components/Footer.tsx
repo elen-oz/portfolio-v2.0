f775b417885296ec3e8aeff38260924057994744
elen-oz <ozerova.lenka@gmail.com>
2024-02-18 18:30:35 +0100

    feat: add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,20 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className='absolute bottom-[0.5rem] right-0 left-0 px-8 flex justify-between'>
+      <ul className='flex gap-1'>
+        <li>Elena Ozerova /</li>
+        <li>Frontend Developer /</li>
+        <li>Stockholm, Sweden</li>
+      </ul>
+
+      <a
+        href='https://github.com/elen-oz/portfolio-v2.0'
+        target='_blanc'
+        rel='nofollow'
+      >
+        /source code/
+      </a>
+    </footer>
+  );
 };
 export default Footer;
