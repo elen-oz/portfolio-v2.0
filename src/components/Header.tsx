95b20b15ee09fb9d5d4390708f06e536d58ba810
elen-oz <ozerova.lenka@gmail.com>
2024-02-18 19:19:26 +0100

    feat: add hover for links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,32 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
+  const handleClickToHome = () => {
+    const element = document.getElementById('top');
+    element!.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handleClickToSkills = () => {
+    const element = document.getElementById('skills');
+    element!.scrollIntoView({ behavior: 'smooth' });
+  };
+  const handleClickToWork = () => {
+    const element = document.getElementById('work');
+    element!.scrollIntoView({ behavior: 'smooth' });
+  };
+  const handleClickToContact = () => {
+    const element = document.getElementById('contact');
+    element!.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <header className='fixed top-0 left-0 right-0 z-10 text-xl md:py-[30px] md:text-[1.7rem]'>
       <div className='mx-auto py-2 flex place-content-between'>
-        <a className='pl-8 flex gap-2 bg-white h-[49px]'>
+        <Link
+          to='/'
+          className='pl-8 flex gap-2 bg-white h-[49px] hover:text-yellow-400'
+          onClick={handleClickToHome}
+        >
           <div className=''>
             <div>elena.</div>
             <div>2024</div>
@@ -11,18 +35,34 @@ const Header = () => {
           <div className='relative'>
             <span>portfolio</span>
 
-            <div className='absolute text-xl bottom-[-7px] md:bottom-[-8px] right-0 self-end px-[11px] pb-[3px] border-solid border-black border-2 md:border-[2.5px] rounded-full'>
+            <div className='absolute text-xl bottom-[-7px] md:bottom-[-8px] right-0 self-end px-[11px] pb-[3px] border-solid border-black border-2 md:border-[2.5px] rounded-full text-black'>
               c
             </div>
           </div>
-        </a>
+        </Link>
 
         <nav className='px-8 bg-white flex md:items-center'>
           <ul className='flex flex-col  text-right md:flex-row md:gap-2 '>
-            <li>Home</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li className='hover:text-sky-300'>
+              <Link to='/' onClick={handleClickToHome}>
+                Home
+              </Link>
+            </li>
+            <li className='hover:text-rose-500'>
+              <Link to='/' onClick={handleClickToSkills}>
+                Skills
+              </Link>
+            </li>
+            <li className='hover:text-fuchsia-300'>
+              <Link to='/' onClick={handleClickToWork}>
+                Work
+              </Link>
+            </li>
+            <li className='hover:text-green-600'>
+              <Link to='/' onClick={handleClickToContact}>
+                Contact
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
