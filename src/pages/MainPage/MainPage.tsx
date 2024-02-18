79bac04f341243b91aa7bbae74a48b8bc9856a75
elen-oz <ozerova.lenka@gmail.com>
2024-02-18 20:53:47 +0100

    feat: add about section

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,16 @@
 import HeroSection from './sections/HeroSection';
 import SkillsSection from './sections/SkillsSection';
-import WorkSection from './sections/WorkSection';
+import LinksSection from './sections/LinksSection';
 import ContactSection from './sections/ContactSection';
+import AboutSection from './sections/AboutSection';
 
 const MainPage = () => {
   return (
     <div id='top'>
       <HeroSection />
+      <AboutSection />
       <SkillsSection />
-      <WorkSection />
+      <LinksSection />
       <ContactSection />
     </div>
   );
