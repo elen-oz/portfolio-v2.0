a8b7214428d8a1ad1b49d12994c0215e49a2ac8a
elen-oz <ozerova.lenka@gmail.com>
2024-02-18 20:54:41 +0100

    feat: add skills section

--- src/pages/MainPage/sections/SkillsSection.tsx
+++ src/pages/MainPage/sections/SkillsSection.tsx
@@ -1,7 +1,17 @@
 const SkillsSection = () => {
   return (
     <section className='h-screen' id='skills'>
-      SkillsSection
+      {/* <h2>(Skills)</h2> */}
+      <div className='flex pf-container sm:px-0 justify-between sm:justify-evenly h-full'>
+        <div className='w-[30px] h-full bg-sky'></div>
+        <div className='w-[30px] h-full bg-sky'></div>
+        <div className='w-[30px] h-full bg-sky'></div>
+        <div className='w-[30px] h-full bg-sky'></div>
+        <div className='w-[30px] h-full bg-sky hidden sm:block'></div>
+        <div className='w-[30px] h-full bg-sky hidden sm:block'></div>
+        <div className='w-[30px] h-full bg-sky hidden sm:block'></div>
+        <div className='w-[30px] h-full bg-sky hidden sm:block'></div>
+      </div>
     </section>
   );
 };
