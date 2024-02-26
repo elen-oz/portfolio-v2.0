2751dfab20e7af9bfb9f2d9d5dad295c0fbaa031
elen-oz <ozerova.lenka@gmail.com>
2024-02-26 22:17:07 +0100

    feat: add data

--- src/data.ts
+++ src/data.ts
@@ -1,14 +1,17 @@
 import rssApp from './assets/projects/RSS-App.webp';
 import diffCalc from './assets/projects/diff-calc.webp';
 import brainGames from './assets/projects/brain-games.webp';
+import Dojo from './assets/projects/Dojo.webp';
+import ReactMeals from './assets/projects/ReactMeals.webp';
+import todayILearned from './assets/projects/today-i-learned.webp';
 
 export type Project = {
-  id: number,
-  name: string,
-  image: string,
-  github: string,
-  live: string,
-  description: string,
+  id: number;
+  name: string;
+  image: string;
+  github: string;
+  live: string;
+  description: string;
 };
 
 const data: Project[] = [
@@ -38,6 +41,33 @@ const data: Project[] = [
     live: '',
     description:
       'Brain Games is a console app with collection of five mini-games designed to challenge the users mental agility. Each game begins with a greeting, personalized with the users name, and consists of three rounds.',
+  },
+  {
+    id: 4,
+    name: 'The Dojo',
+    image: Dojo,
+    github: 'https://github.com/elen-oz/the-dojo',
+    live: 'https://thedojosite-355e3.web.app/',
+    description:
+      'The Dojo is a collaborative project management tool that allows users to register, invite colleagues, and work together on projects. This application is built with React.js, CSS, and HTML, and uses Firebase for backend services. If you do not want to create user: login: jake@email.com, password: 123123',
+  },
+  {
+    id: 5,
+    name: 'Food Order App',
+    image: ReactMeals,
+    github: 'https://github.com/elen-oz/food-order-app',
+    live: 'https://food-order-app-9b4ad.web.app/',
+    description:
+      'This is a food ordering application built with React.js, CSS, and HTML. The application allows users to browse through a menu, select their desired items, and place an order.',
+  },
+  {
+    id: 6,
+    name: 'Today I learned',
+    image: todayILearned,
+    github: 'https://github.com/elen-oz/today-i-learned-app',
+    live: 'https://today-i-learned-app-mauve.vercel.app/',
+    description:
+      'Today I Learned is a web application where users can share interesting facts and vote for others. This project is built with React and uses Supabase for backend services.',
   },
 ];
 
