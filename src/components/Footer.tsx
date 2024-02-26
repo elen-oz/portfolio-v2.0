6c317c5fefded668a4ff903a303baec77645e46f
elen-oz <ozerova.lenka@gmail.com>
2024-02-26 22:51:52 +0100

    feat: update layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className='absolute bottom-0 right-0 left-0 px-4 pb-2 md:px-8 flex gap-1 justify-between md:text-xl text-stone-600'>
+    <footer className='absolute bottom-0 right-0 left-0 px-4  md:px-8 flex gap-1 justify-between text-xl text-stone-600'>
       <a
-        className='hover:text-green underline uppercase'
+        className='hover:text-green underline'
         href='https://github.com/elen-oz/portfolio-v2.0'
         target='_blanc'
         rel='nofollow'
@@ -11,7 +11,9 @@ const Footer = () => {
       </a>
 
       <ul className='flex gap-1'>
-        <li>Elena Ozerova /</li>
+        <li>
+          Elena<span className='hidden sm:block'> Ozerova</span>/
+        </li>
         <li className='hidden sm:block'>Frontend Developer /</li>
         <li className='hidden md:block'>Stockholm, Sweden</li>
       </ul>
