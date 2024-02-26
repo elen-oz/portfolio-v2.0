324fc45f2cc209ddbdf3a54347a9273963716643
elen-oz <ozerova.lenka@gmail.com>
2024-02-26 11:23:32 +0100

    refactor

--- src/pages/MainPage/sections/AboutSection.tsx
+++ src/pages/MainPage/sections/AboutSection.tsx
@@ -4,13 +4,13 @@ const AboutSection = () => {
       <h2 className='text-6xl text-center pt-4'>/About/</h2>
       <div className='h-full flex flex-col justify-center items-center text-center'>
         <ul className='max-w-[700px] mx-auto flex justify-center gap-1 text-xl md:text-3xl flex-wrap uppercase'>
-          <li className='whitespace-nowrap'>&#9737;ACRYLIC ARTIST</li>
-          <li className='whitespace-nowrap'>&#9737;CROSSFIT ENTHUSIAST</li>
-          <li className='whitespace-nowrap'>&#9737;HIKING EXPLORER</li>
+          <li className='whitespace-nowrap'>&#9737;Acrylic artist</li>
+          <li className='whitespace-nowrap'>&#9737;Crossfit enthusiast</li>
+          <li className='whitespace-nowrap'>&#9737;Hiking explorer</li>
           <li className='whitespace-nowrap'>&#9737;war hater</li>
-          <li className='whitespace-nowrap'>&#9737;RETRO POP FAN</li>
-          <li className='whitespace-nowrap'>&#9737;PLANT LOVER</li>
-          <li className='whitespace-nowrap'>&#9737;DOG PERSON</li>
+          <li className='whitespace-nowrap'>&#9737;Retro pop fan</li>
+          <li className='whitespace-nowrap'>&#9737;Plant lover</li>
+          <li className='whitespace-nowrap'>&#9737;Dog person</li>
         </ul>
       </div>
     </section>
