ed230c37d76cf4cf6ec63e5549b42f3c7e442440
elen-oz <ozerova.lenka@gmail.com>
2024-02-28 17:51:39 +0100

    feat: footer: change text-color

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -24,7 +24,7 @@ const data: Project[] = [
     live: 'https://frontend-project-11-01.vercel.app/',
     description:
       'RSS Aggregator is a specialized tool designed to collate news feeds, article announcements, and other content from various websites. It provides an easy way for users to subscribe to updates and view them in a user-friendly manner. This project is built with pure JavaScript and uses the Bootstrap library for styling.',
-    details: 'project details here',
+    details: 'project details will be here soon...',
   },
   {
     id: 2,
@@ -34,7 +34,7 @@ const data: Project[] = [
     live: '',
     description:
       'Difference Calculator is a console application that designed to determine the difference between two data structures. It supports various file formats including .json and .yaml, and can handle both flat and nested structures.',
-    details: 'project details here',
+    details: 'project details will be here soon...',
   },
   {
     id: 3,
@@ -44,7 +44,7 @@ const data: Project[] = [
     live: '',
     description:
       'Brain Games is a console app with collection of five mini-games designed to challenge the users mental agility. Each game begins with a greeting, personalized with the users name, and consists of three rounds.',
-    details: 'project details here',
+    details: 'project details will be here soon...',
   },
   {
     id: 4,
@@ -54,7 +54,7 @@ const data: Project[] = [
     live: 'https://thedojosite-355e3.web.app/',
     description:
       'The Dojo is a collaborative project management tool that allows users to register, invite colleagues, and work together on projects. This application is built with React.js, CSS, and HTML, and uses Firebase for backend services. If you do not want to create user: login: jake@email.com, password: 123123',
-    details: 'project details here',
+    details: 'project details will be here soon...',
   },
   {
     id: 5,
@@ -64,7 +64,7 @@ const data: Project[] = [
     live: 'https://food-order-app-9b4ad.web.app/',
     description:
       'This is a food ordering application built with React.js, CSS, and HTML. The application allows users to browse through a menu, select their desired items, and place an order.',
-    details: 'project details here',
+    details: 'project details will be here soon...',
   },
   {
     id: 6,
@@ -74,7 +74,7 @@ const data: Project[] = [
     live: 'https://today-i-learned-app-mauve.vercel.app/',
     description:
       'Today I Learned is a web application where users can share interesting facts and vote for others. This project is built with React and uses Supabase for backend services.',
-    details: 'project details here',
+    details: 'project details will be here soon...',
   },
 ];
 
