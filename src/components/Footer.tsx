cdf51001aa40b0165d0d617e18a9204dc874225a
elen-oz <ozerova.lenka@gmail.com>
2024-02-28 23:26:05 +0100

    feat: change footer link color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className='absolute bottom-0 right-0 left-0 px-4  md:px-8 flex gap-1 justify-between text-xl'>
       <a
-        className='hover:text-green underline'
+        className='hover:text-blue underline'
         href='https://github.com/elen-oz/portfolio-v2.0'
         target='_blanc'
         rel='nofollow'
