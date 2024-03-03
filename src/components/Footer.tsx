d2fe4d52a158b87aac9775729d609f67287d24f5
elen-oz <ozerova.lenka@gmail.com>
2024-03-03 22:31:01 +0100

    feat: change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ const Footer = () => {
   return (
     <footer className='absolute bottom-0 left-0 right-0 flex  justify-end gap-1 px-4 text-xl md:px-8'>
       <ul className='flex gap-1'>
-        <li>Elena Ozerova/</li>
+        <li>
+          Elena<span className='hidden min-[400px]:inline'> Ozerova</span>/
+        </li>
         <li>Frontend Developer/</li>
         <li className='hidden md:block'>Stockholm, Sweden</li>
       </ul>
