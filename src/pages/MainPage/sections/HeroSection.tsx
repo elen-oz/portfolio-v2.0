596e76eca4696a1c6229bea93afe1647a5a38a15
elen-oz <ozerova.lenka@gmail.com>
2024-03-09 13:29:40 +0100

    feat: chanhe hero section: remove paddings

--- src/pages/MainPage/sections/HeroSection.tsx
+++ src/pages/MainPage/sections/HeroSection.tsx
@@ -3,9 +3,9 @@ import photoMdImg from '../../../assets/hero-md.webp';
 
 const HeroSection = () => {
   return (
-    <section className='flex min-h-screen flex-col justify-end px-4 pt-[100px] md:px-8 md:pb-8 md:pt-0'>
-      <div className='flex flex-col justify-between md:flex-row md:gap-2'>
-        <div className='flex flex-1 flex-col justify-end'>
+    <section className='flex min-h-screen flex-col justify-end px-4 pt-[100px] md:pb-4 md:pt-0'>
+      <div className='flex flex-col justify-between md:flex-row md:justify-between md:gap-2'>
+        <div className='flex flex-1 flex-col justify-end md:pb-4'>
           <p className='pb-2 lg:text-3xl'>HELLO, I'M</p>
           <h1 className='max-w-64 pb-2 text-4xl leading-normal md:text-3xl md:leading-normal lg:text-6xl lg:leading-normal'>
             ELENA OZEROVA
@@ -17,7 +17,7 @@ const HeroSection = () => {
         <div className='relative flex-1 pt-[50px] md:pt-0'>
           <div className='absolute left-[50px] top-[10px] h-[100px] w-[100px] rounded-full bg-red md:left-[-7%] md:top-[35%] md:h-[150px] md:w-[150px]' />
 
-          <picture>
+          <picture className='md:flex md:justify-end'>
             <source srcSet={photoImg} media='(min-width: 768px)' />
             <img
               src={photoMdImg}
