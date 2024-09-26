9bb397cb9ad1b87f0dbae83c632fb6787c06f019
elen-oz <ozerova.lenka@gmail.com>
2024-09-26 20:11:26 +0200

    feat: add mobile toggle menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { TfiClose, TfiMenu } from 'react-icons/tfi';
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   const handleClickToHome = () => {
     const element = document.getElementById('top');
     element?.scrollIntoView({ behavior: 'smooth' });
@@ -8,7 +16,7 @@ const Header = () => {
 
   return (
     <header className='content-container fixed left-0 right-0 top-0 z-10 text-xl md:text-[1.7rem]'>
-      <div className='mx-auto flex place-content-between py-2'>
+      <div className='mx-auto flex place-content-between pb-2'>
         <Link
           to='/'
           className='relative grid h-full grid-cols-[auto,1fr] grid-rows-[29px] gap-x-2 bg-white pl-8 hover:text-yellow'
@@ -22,8 +30,9 @@ const Header = () => {
           </div>
         </Link>
 
-        <nav className='flex bg-white px-8 md:items-center'>
-          <ul className='flex flex-col  text-right md:flex-row md:gap-2 '>
+        {/* -------- desktop menu -------- */}
+        <nav className='hidden bg-white px-8 md:flex md:items-center'>
+          <ul className='flex flex-row gap-2 text-right '>
             <li className='hover:text-sky'>
               <Link to='/' onClick={handleClickToHome}>
                 Home
@@ -36,6 +45,34 @@ const Header = () => {
               <Link to='/contact'>Contact</Link>
             </li>
           </ul>
+        </nav>
+
+        {/* -------- mobile menu -------- */}
+        <nav className='flex bg-white md:hidden'>
+          {isMenuOpen ? (
+            <ul className='flex flex-col text-right'>
+              <li className='md:hidden p-6'>
+                <button onClick={toggleMenu}>
+                  <TfiClose />
+                </button>
+              </li>
+              <li className='hover:text-sky p-4'>
+                <Link to='/' onClick={handleClickToHome}>
+                  Home
+                </Link>
+              </li>
+              <li className='hover:text-purple p-4'>
+                <Link to='/work'>Work</Link>
+              </li>
+              <li className='hover:text-green p-4'>
+                <Link to='/contact'>Contact</Link>
+              </li>
+            </ul>
+          ) : (
+            <button onClick={toggleMenu} className='p-6'>
+              <TfiMenu />
+            </button>
+          )}
         </nav>
       </div>
     </header>
