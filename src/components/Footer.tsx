57813d2c7bfb4ce08cebd8d82a17e7485657f55f
elen-oz <ozerova.lenka@gmail.com>
2024-09-26 18:53:00 +0200

    feat: change justify

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className='content-container absolute bottom-0 left-0 right-0 flex justify-end gap-1 px-4 text-xl md:px-8'>
-      <ul className='flex gap-1'>
+    <footer className='content-container absolute bottom-0 left-0 right-0 flex justify-center gap-1 px-4 text-xl md:px-8'>
+      <ul className='flex gap-1 bg-white'>
         <li>
           Elena<span className='hidden min-[400px]:inline'> Ozerova</span>/
         </li>
