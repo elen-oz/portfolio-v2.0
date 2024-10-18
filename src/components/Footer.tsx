d9f28f84ac76b76f4b3bb85456a4699624dc63f1
elen-oz <ozerova.lenka@gmail.com>
2024-10-18 14:13:38 +0200

    small fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     const { t } = useTranslation('global');
 
     return (
-    <footer className='w-full bg-white content-container absolute bottom-0 left-0 right-0 flex justify-center gap-1 px-4 text-xl md:px-8'>
+    <footer className='w-full bg-white content-container  flex justify-center gap-1 px-4 text-xl md:px-8'>
       <ul className='flex gap-1 '>
         <li>
           Elena<span className='hidden min-[400px]:inline'> Ozerova</span>/
--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -21,61 +21,59 @@ const ContactPage = () => {
   }, [closeMenu]);
 
   return (
-    <div className='z-90 scrollbar-hide-y relative h-screen overflow-y-auto bg-yellow uppercase'>
-      <div className='sticky left-8 top-[100px] px-4 md:px-8 min-[1600px]:translate-x-[10rem] min-[2000px]:translate-x-[15rem] min-[2300px]:translate-x-[25rem]'>
-        <ul className='text-4xl sm:text-6xl'>
-          <li className='hover:text-white'>
-            <Link to='/work'>{t('work')}</Link>
-          </li>
-          <li className='hover:text-white'>
-            <a
-              href='https://rebrand.ly/elen-oz-cv'
-              target='_blank'
-              rel='noreferrer'
+    <div className='relative h-screen overflow-hidden bg-yellow uppercase'>
+      <ul className='flex flex-col items-start justify-center pl-[70px] pt-[100px] text-4xl sm:text-6xl'>
+        <li className='hover:text-white'>
+          <Link to='/work'>{t('work')}</Link>
+        </li>
+        <li className='hover:text-white'>
+          <a
+            href='https://rebrand.ly/elen-oz-cv'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {t('resume')}
+          </a>
+        </li>
+
+        <li className='text-lg'>&rsaquo; {t('description')} &rsaquo;</li>
+
+        <li className='hover:text-white'>
+          <a href='mailto: ozerova.lenka@gmail.com?subject=Mail from Portfolio Site'>
+            {t('email')}
+          </a>
+        </li>
+
+        <li className='hover:text-white'>
+          <a
+            href='https://www.linkedin.com/in/elen-oz/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {t('linkedin')}
+          </a>
+        </li>
+
+        <li className='hover:text-white'>
+          <a
+            href='https://github.com/elen-oz'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {t('github')}
+          </a>
+        </li>
+        <li className='hover:text-white'>
+          <CopyToClipboard text='@elenoz'>
+            <div
+              className='max-w-[200px] text-left sm:max-w-[250px]'
+              onClick={onCopy}
             >
-              {t('resume')}
-            </a>
-          </li>
-
-          <li className='text-lg'>&rsaquo; {t('description')} &rsaquo;</li>
-
-          <li className='hover:text-white'>
-            <a href='mailto: ozerova.lenka@gmail.com?subject=Mail from Portfolio Site'>
-              {t('email')}
-            </a>
-          </li>
-
-          <li className='hover:text-white'>
-            <a
-              href='https://www.linkedin.com/in/elen-oz/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              {t('linkedin')}
-            </a>
-          </li>
-
-          <li className='hover:text-white'>
-            <a
-              href='https://github.com/elen-oz'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {t('github')}
-            </a>
-          </li>
-          <li className='hover:text-white'>
-            <CopyToClipboard text='@elenoz'>
-              <div
-                className='max-w-[200px] text-left sm:max-w-[250px]'
-                onClick={onCopy}
-              >
-                {copied ? t('copy-text') : t('telegram')}
-              </div>
-            </CopyToClipboard>
-          </li>
-        </ul>
-      </div>
+              {copied ? t('copy-text') : t('telegram')}
+            </div>
+          </CopyToClipboard>
+        </li>
+      </ul>
 
       <motion.div
         className='absolute hidden bg-red sm:bottom-[-90%] sm:right-[-30%] sm:block sm:h-[500px] sm:w-[500px] md:h-[1000px] md:w-[1000px]'
@@ -94,15 +92,6 @@ const ContactPage = () => {
           ease: 'linear',
         }}
       />
-
-      {/*<div*/}
-      {/*    className='fixed text-md text-end text-gray-500 min-[600px]:text-start min-[600px]:text-2xl  bottom-16 right-10  min-[600px]:bottom-36  min-[600px]:translate-x-[1rem]   min-[600px]:rotate-90 min-[600px]:transform    min-[2500px]:translate-x-[-26rem]'>*/}
-      {/*  scroll for fun*/}
-      {/*</div>*/}
-
-      {/*{bgColors.map((color, index) => (*/}
-      {/*    <div key={index} className={`h-full ${color}`}/>*/}
-      {/*))}*/}
     </div>
   );
 };
